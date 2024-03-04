a7569fb12fe8d837539748e586a92044dd6dece1
Jon <jonathan@dtacgear.net>
2024-03-04 07:37:48 -0500

    additional predictions generated

--- test.py
+++ test.py
@@ -1,6 +1,32 @@
 from hmm_training import StockPredictor
+import pickle
 
-stockpred = StockPredictor(('zts', '1_day'), multimodal=False)
-stockpred.fit()
-mape = stockpred.predict_close_prices_for_days(252, with_plot=True)
-print(mape)
+data = pickle.load(open('mapes.pkl', 'rb'))
+data1 = pickle.load(open('mapes1.pkl', 'rb'))
+data2 = pickle.load(open('mapes2.pkl', 'rb'))
+data3 = pickle.load(open('mapes3.pkl', 'rb'))
+data4 = pickle.load(open('mapes4.pkl', 'rb'))
+
+print(data3)
+
+
+# mapes = [data1,data2,data3,data4]
+
+# print(len(data))
+
+# for arr in mapes:
+#     for elem in arr:
+#         if elem in data:
+#             continue
+#         data.append(elem)
+
+# print(len(data))
+
+# print(data)
+
+
+# stockpred = StockPredictor(('zts', '1_day'), multimodal=False)
+# stockpred.fit()
+# mape = stockpred.predict_close_prices_for_days(252, with_plot=True)
+# print(mape)
+
