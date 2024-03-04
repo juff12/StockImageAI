a5b6189e685f2df9c382435de5ecfc1525241716
Jon <jonathan@dtacgear.net>
2024-03-04 15:52:54 -0500

    reformatted predicted data, moved arrays to pkl files

--- test.py
+++ test.py
@@ -1,18 +1,20 @@
 from hmm_training import StockPredictor
 import pickle
+from pathlib import Path
+import pandas as pd
 
 data = pickle.load(open('mapes.pkl', 'rb'))
-data1 = pickle.load(open('mapes1.pkl', 'rb'))
-data2 = pickle.load(open('mapes2.pkl', 'rb'))
-data3 = pickle.load(open('mapes3.pkl', 'rb'))
-data4 = pickle.load(open('mapes4.pkl', 'rb'))
+# data1 = pickle.load(open('mapes1.pkl', 'rb'))
+# data2 = pickle.load(open('mapes2.pkl', 'rb'))
+# data3 = pickle.load(open('mapes3.pkl', 'rb'))
+# data4 = pickle.load(open('mapes4.pkl', 'rb'))
 
-print(data3)
+#print(data4)
 
 
-# mapes = [data1,data2,data3,data4]
+#mapes = [data1,data2,data3,data4]
 
-# print(len(data))
+print(len(data))
 
 # for arr in mapes:
 #     for elem in arr:
@@ -20,9 +22,13 @@ print(data3)
 #             continue
 #         data.append(elem)
 
-# print(len(data))
+# with open('mapes.pkl', 'wb') as file:
+#     pickle.dump(data, file)
 
-# print(data)
+df_mape = pd.DataFrame(data, columns=['ticker','1_day','4_hour','1_hour'])
+filepath = Path('data/mapes/sp500_mapes.csv')
+filepath.parent.mkdir(parents=True, exist_ok=True)
+df_mape.to_csv(filepath,index=False)
 
 
 # stockpred = StockPredictor(('zts', '1_day'), multimodal=False)
